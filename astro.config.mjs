60b8710eda1e57c9da409dbfd62a596686f26c48
eno? <enoabasidream@gmail.com>
2025-10-11 13:47:06 +0100

    add fonts + setup tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import react from '@astrojs/react';
+
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
