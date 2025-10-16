fca7602a989f35bbd5dfb14ce90fab1b60e29c92
eno? <enoabasidream@gmail.com>
2025-10-16 22:22:10 +0100

    feat: switch deployment to netlify from cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import cloudflare from '@astrojs/cloudflare';
-
 import react from '@astrojs/react';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare(),
   integrations: [react()],
-
   vite: {
     plugins: [tailwindcss()]
   }
