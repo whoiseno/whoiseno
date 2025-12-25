d9ebd19ec4a856b2eb11abb27e8c3fedb2ca111a
eno? <enoabasidream@gmail.com>
2025-12-25 12:22:20 +0100

    add keystatic cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,17 @@
 // @ts-check
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc";
+import keystatic from "@keystatic/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
-
-  devToolbar: {
-    enabled: false
-  },
-
-  integrations: [react()]
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	integrations: [react(), markdoc(), keystatic()],
+	devToolbar: {
+		enabled: false,
+	},
 });
--- src/components/react.tsx
+++ src/components/react.tsx
@@ -1,5 +1,3 @@
 export default function React() {
-  return (
-    <p className="text-blue-400">This is from React</p>
-  )
+	return <p className="text-blue-400">This is from React</p>;
 }
