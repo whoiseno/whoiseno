acb0ca0d7ebe548031c7bc4c549572b993204c4c
eno? <enoabasidream@gmail.com>
2025-12-25 12:04:31 +0100

    add react + fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   devToolbar: {
     enabled: false
-  }
+  },
+
+  integrations: [react()]
 });
---
+++ src/components/react.tsx
@@ -0,0 +1,5 @@
+export default function React() {
+  return (
+    <p className="text-blue-400">This is from React</p>
+  )
+}
