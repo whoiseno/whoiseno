14e5f999cd8f9ca5e4f5b61b79f315b8c6030851
eno? <enoabasidream@gmail.com>
2026-01-15 14:20:04 +0100

    Landing page v1
    Landing page version 1 completed

---
+++ src/config.ts
@@ -0,0 +1,14 @@
+export const config = {
+	site: "https://enoabasiessien.netlify.app",
+	title: "Whoiseno | Enoabasi Essien",
+	description: "A software developer & product designer building aesthetic, user-centric interfaces.",
+	author: {
+		name: "Enoabasi Essien",
+		email: "humanwhodreams@gmail.com",
+	},
+	socials: {
+		github: { url: "https://github.com/whoiseno", handle: "@whoiseno" },
+		twitter: { url: "https://x.com/enoiscooked", handle: "@enoiscooked" },
+		linkedin: { url: "https://linkedin.com/enoabasi-essien", handle: "@enoabasi-essien" },
+	},
+}
