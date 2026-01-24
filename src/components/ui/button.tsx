a81fab6647b4141ab23bb55e6a3db6c39ae2ab8f
eno? <enoabasidream@gmail.com>
2026-01-24 14:43:35 +0100

    add new secondary ghost variant

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -14,6 +14,8 @@ const buttonVariants = cva(
 					"border-border bg-background hover:bg-muted hover:text-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 aria-expanded:bg-muted aria-expanded:text-foreground",
 				secondary:
 					"bg-secondary text-secondary-foreground hover:bg-secondary/80 aria-expanded:bg-secondary aria-expanded:text-secondary-foreground",
+				"secondary-ghost":
+					"text-secondary-foreground hover:bg-secondary/80 dark:hover:bg-secondary/80 aria-expanded:bg-secondary aria-expanded:text-secondary-foreground",
 				ghost:
 					"hover:bg-muted hover:text-foreground dark:hover:bg-muted/50 aria-expanded:bg-muted aria-expanded:text-foreground",
 				destructive:
