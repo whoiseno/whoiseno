ce139080aae8511e71aebd1c1f359ace93e31a7f
eno? <enoabasidream@gmail.com>
2026-01-24 23:10:29 +0100

    content management layer with keystatic and astro

---
+++ keystatic.config.ts
@@ -0,0 +1,42 @@
+import { collection, config, fields } from "@keystatic/core";
+
+export default config({
+	storage: {
+		kind: "local",
+	},
+	collections: {
+		posts: collection({
+			label: "Posts",
+			path: "src/content/posts/*/",
+			entryLayout: "content",
+			format: {
+				contentField: "content",
+			},
+			schema: {
+				title: fields.slug({ name: { label: "Title", description: "The title of the post." } }),
+				summary: fields.text({ label: "Summary", description: "The summary of the post.", multiline: true }),
+				draft: fields.checkbox({
+					label: "Draft",
+					description: "Prevent this post from being published.",
+					defaultValue: false,
+				}),
+				createdAt: fields.datetime({
+					label: "Created At",
+					description: "The date the post was created.",
+					defaultValue: {
+						kind: "now",
+					},
+				}),
+				updatedAt: fields.datetime({
+					label: "Updated At",
+					description: "The date the post was updated.",
+					defaultValue: {
+						kind: "now",
+					},
+				}),
+				content: fields.markdoc({ label: "Content", extension: "mdoc" }),
+			},
+			slugField: "title",
+		}),
+	},
+});
--- src/content.config.ts
+++ src/content.config.ts
@@ -4,12 +4,13 @@ import { z } from "astro/zod";
 
 const posts = defineCollection({
 	loader: glob({ pattern: "**/*.mdoc", base: "./src/content/posts" }),
-  schema: z.object({
-    title: z.string(),
-    summary: z.string(),
-    createdAt: z.coerce.date(),
-    updatedAt: z.coerce.date().optional()
-  })
+	schema: z.object({
+		title: z.string(),
+		summary: z.string(),
+		draft: z.boolean().default(false),
+		createdAt: z.coerce.date(),
+		updatedAt: z.coerce.date().optional(),
+	}),
 });
 
 export const collections = { posts };
