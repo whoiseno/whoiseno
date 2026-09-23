e06cc2ea22a566cf167b3e887882b0aac33999de
Enoabasi Essien <enoabasidream@gmail.com>
2026-09-23 13:38:53 +0100

    add fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import alpinejs from "@astrojs/alpinejs";
 import partytown from "@astrojs/partytown";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,4 +11,40 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  fonts: [
+    {
+    provider: fontProviders.local(),
+    name: "Supreme",
+    cssVariable: "--font-supreme-variable",
+    options: {
+      variants: [
+        {
+          weight: "100 900",
+          style: "normal",
+          src: ["./src/app/fonts/Supreme-Variable.woff2"]
+        }
+      ]
+    }
+  },
+  {
+    provider: fontProviders.local(),
+    name: "Chubbo",
+    cssVariable: "--font-chubbo-variable",
+    options: {
+      variants: [
+        {
+          weight: "100 900",
+          style: "normal",
+          src: ["./src/app/fonts/Chubbo-Variable.woff2"]
+        }
+      ]
+    }
+  },
+  {
+    provider: fontProviders.fontsource(),
+    name: "Geist Mono",
+    cssVariable: "--font-geist-mono",
+  }
+]
 });
