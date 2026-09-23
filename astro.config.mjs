96ed218be26f5553fcb4f75f961205a797542cec
Enoabasi Essien <enoabasidream@gmail.com>
2026-09-23 11:02:44 +0100

    add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import alpinejs from '@astrojs/alpinejs';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs({ entrypoint: "/src/entrypoint" })]
+  integrations: [alpinejs({ entrypoint: "/src/entrypoint" }), partytown()]
 });
