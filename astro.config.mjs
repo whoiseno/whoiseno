75f83984cec7d8f791c5b6e446cd4db78e05c5ba
Enoabasi Essien <enoabasidream@gmail.com>
2026-09-23 12:40:49 +0100

    feature-slice design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 import alpinejs from "@astrojs/alpinejs";
 import partytown from "@astrojs/partytown";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs({ entrypoint: "/src/entrypoint" }), partytown()],
+  integrations: [alpinejs({ entrypoint: "/src/app/entrypoints/alpine" }), partytown()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
