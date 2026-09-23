4397cc60fe472622bff76e76425dc2ea89d8cc20
Enoabasi Essien <enoabasidream@gmail.com>
2026-09-23 10:59:36 +0100

    add alpinejs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [alpinejs()]
+  integrations: [alpinejs({ entrypoint: "/src/entrypoint" })]
 });
